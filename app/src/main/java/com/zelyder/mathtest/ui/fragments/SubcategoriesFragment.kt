f142a949bcea2b564918a4fca3ffc9d4b7aaa02a
Zelyder <57kaa@mail.ru>
2020-05-05 23:27:44 +0300

    Interstitial ad added

--- app/src/main/java/com/zelyder/mathtest/ui/fragments/SubcategoriesFragment.kt
+++ app/src/main/java/com/zelyder/mathtest/ui/fragments/SubcategoriesFragment.kt
@@ -1,30 +1,30 @@
 package com.zelyder.mathtest.ui.fragments
 
-import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.yandex.mobile.ads.AdRequest
+import com.yandex.mobile.ads.AdRequestError
+import com.yandex.mobile.ads.InterstitialAd
+import com.yandex.mobile.ads.InterstitialEventListener
+import com.yandex.mobile.ads.InterstitialEventListener.SimpleInterstitialEventListener
 import com.zelyder.mathtest.R
 import com.zelyder.mathtest.data.viewmodels.MainViewModel
-import com.zelyder.mathtest.data.viewmodels.PageViewModel
-import com.zelyder.mathtest.help.ARG_CATEGORY_ID
-import com.zelyder.mathtest.help.ARG_INDEX_BACK
-import com.zelyder.mathtest.help.ARG_SECTION_ID
-import com.zelyder.mathtest.help.ARG_SUBCATEGORY_ID
 import com.zelyder.mathtest.ui.adapters.SubcategoriesListAdapter
 
 class SubcategoriesFragment : Fragment() {
 
     val TAG = "SubcategoriesFragment"
     var isBack = false
+    lateinit var mAdRequest: AdRequest
+    lateinit var mInterstitialAd: InterstitialAd
 
     private val mainViewModel by lazy { ViewModelProviders.of(this)
         .get(MainViewModel::class.java) }
@@ -42,8 +42,10 @@ class SubcategoriesFragment : Fragment() {
 
         val args = arguments?.let { SubcategoriesFragmentArgs.fromBundle(it) }
 
+        initInterstitialAd()
+
         if(args?.indexBack == 1 && !isBack){
-            Log.d(TAG, "Count Back ${args.indexBack}  categoryId ${args.categoryId}")
+            mInterstitialAd.loadAd(mAdRequest)
             isBack = true
         }
 
@@ -64,5 +66,26 @@ class SubcategoriesFragment : Fragment() {
 
         return root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mInterstitialAd.destroy()
+    }
+
+    private fun initInterstitialAd() {
+        mInterstitialAd = InterstitialAd(requireContext())
+        mInterstitialAd.blockId = resources.getString(R.string.interstitial_ad_yandex_meditation_id)
+        mAdRequest = AdRequest.builder().build()
+        mInterstitialAd.interstitialEventListener = mInterstitialAdEventListener
+    }
+
+    private val mInterstitialAdEventListener: InterstitialEventListener =
+        object : SimpleInterstitialEventListener() {
+            override fun onInterstitialLoaded() {
+                mInterstitialAd.show()
+            }
+
+            override fun onInterstitialFailedToLoad(error: AdRequestError) {}
+        }
 }
 
